93480ae8b4a21992c4d1016729a84eb77a272fc0
James <42484571+smitteh1@users.noreply.github.com>
2018-11-11 23:25:06 -0500

    Update players.js

--- routes/players.js
+++ routes/players.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const con = require('../functions/sql');
+
 
 router.get('/', (req, res, next) => {
-	res.render('players', { title: 'Players', user: req.user })
+	con.connect(function(err) {
+		const data = con.query("SELECT * FROM webuser", function (err, result, fields) {
+			res.render('players', { title: 'Players', user: req.user, result })
+		});
+	});
 });
 
 module.exports = router;
